27a4020ef89dd2406c54ae4d6ab722a9cc178d69
developiaa <developiaa@gmail.com>
2021-04-15 10:30:12 +0900

    Add jwt setting, requirements

--- django_jwt_study/settings.py
+++ django_jwt_study/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +129,29 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SIMPLE_JWT = {
+	'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+	'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+	'ROTATE_REFRESH_TOKENS': False,
+	'BLACKLIST_AFTER_ROTATION': True,
+
+	'ALGORITHM': 'HS256',
+	'SIGNING_KEY': 'T2SToFSignInGkEy',
+	'VERIFYING_KEY': None,
+	'AUDIENCE': None,
+	'ISSUER': None,
+
+	'AUTH_HEADER_TYPES': ('Bearer',),
+	'USER_ID_FIELD': 'id',
+	'USER_ID_CLAIM': 'user_id',
+
+	'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+	'TOKEN_TYPE_CLAIM': 'token_type',
+
+	'JTI_CLAIM': 'jti',
+
+	'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+	'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+	'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
